345219f88ea8960709f334dba406fdfaf81b8ed7
ahmad <ahmed.salamony497@gmail.com>
2021-04-15 15:42:30 +0200

    commit before upload to github

---
+++ djackets/product/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .serializers import ProductSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+
+# Create your views here.
+class LatestProductsList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()[:4]
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data) 
